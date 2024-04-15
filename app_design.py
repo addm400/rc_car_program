bc10ada436c0e6a7c0e4ed77c5012ea0ad1a15b9
Adam Laptop <addm400@gmail.com>
2024-04-15 11:21:02 +0200

    trying to resize joystick image

--- app_design.py
+++ app_design.py
@@ -114,12 +114,12 @@ class App(customtkinter.CTk):
 
         self.joystick_board = Canvas(self.third_frame, width=self.canvas_width, height=self.canvas_height, highlightthickness=0)
         self.joystick_board.grid(row=0, column=0, pady=(20, 0), padx=(20, 140), sticky="nsew")
-
+        
         self.bgphoto = PhotoImage(file='test_images//arrows.png')
         self.plansza = self.joystick_board.create_image(0, 0, image=self.bgphoto, anchor=NW)
 
         self.photoimage = PhotoImage(file='test_images//kolko.png')
-        self.joystick_steering_label = self.joystick_board.create_image(0, 0, image=self.photoimage, anchor=NW)
+        self.joystick_steering_label = self.joystick_board.create_image(0, 0, image=self.keyboard_image, anchor=NW)
         self.joystick_board.moveto(self.joystick_steering_label, 98, 97)
 
         self.joystick_height = self.photoimage.height()
